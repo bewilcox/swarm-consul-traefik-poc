1d61c420c22174cc3964e10997cb210a32243516
Benoit Wilcox <wilcox.benoit@gmail.com>
2017-01-20 18:22:38 +0100

    Scripts for create swarm cluster

---
+++ scripts/add-visualizer-service.sh
@@ -0,0 +1,10 @@
+# service Docker qui me permet de visualiser mon cluster
+#########################################################
+eval "$(docker-machine env leader1)"
+
+docker service create \
+  --name=viz \
+  --publish=8080:8080/tcp \
+  --constraint=node.role==manager \
+  --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+  manomarks/visualizer
---
+++ scripts/create-network.sh
@@ -0,0 +1,8 @@
+# Initialisation du réseau
+######################################################################
+
+eval "$(docker-machine env leader1)"
+
+docker network create \
+    -d overlay --subnet 10.1.9.0/24 \
+    multi-host-net
